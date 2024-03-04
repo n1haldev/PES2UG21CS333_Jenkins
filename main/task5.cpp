4256bc3d85d2634cc4448575066b34de5ec9a7e2
Nihal T M <nihaltmdev@gmail.com>
2024-03-04 23:33:52 +0530

    task 5, jenkinsfile

---
+++ main/task5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "This is for task 5" << endl;
+    cout << "Looks like my jenkins pipeline works after all" << endl;
+
+    return 0;
+}
